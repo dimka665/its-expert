69686733d8dba7ba04db454ae29325ca564b1c2c
dimka665 <dimka665@gmail.com>
2014-07-30 21:01:47 +0400

    Added object list

--- its_expert/models.py
+++ its_expert/models.py
@@ -12,5 +12,5 @@ class ObjectX(Model):
             'height': self.height,
             'width': self.width,
         }
-        attrs_str = ', '.join('{}={}'.format(attr_item[0], attr_item[1]) for attr_item in attrs.items())
+        attrs_str = ', '.join('{}={}'.format(*attr_item) for attr_item in attrs.items())
         return 'Object X ({attrs})'.format(attrs=attrs_str)
--- its_expert/views.py
+++ its_expert/views.py
@@ -7,3 +7,9 @@ from its_expert.models import ObjectX
 class CreateObjectXView(CreateView):
     model = ObjectX
     success_url = reverse_lazy('create-object-x')
+
+    def get_context_data(self, **kwargs):
+        object_list = ObjectX.objects.all()
+        context = {'object_list': object_list}
+        context.update(kwargs)
+        return super(CreateObjectXView, self).get_context_data(**context)
